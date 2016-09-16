9fee32077fe171a693b1e72d1402dad843351b82
Rifky Ekayama <rifqykiw@gmail.com>
2016-09-16 19:26:15 +0700

    fix web route

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,24 @@
 |
 */
 
-Route::group(['https' => config('app.https')], function(){
+Route::get('/', 'Front\BerandaController@index');
 
-	Route::get('/', function () {
-	    return view('pages/backend/dashboard')->withTitle('Dashboard');
-	});
-	/* FRONT */
-	Route::group(['namespace' => 'Front'], function(){
-		Route::resource('beranda','BerandaController');
-		Route::resource('faq','FAQController');
-		Route::resource('forum-fasilitator','ForumFasilitatorController');
-		Route::resource('galeri','GaleriController');
-		Route::resource('hubungi-kami','HubungiKamiController');
-		Route::resource('media-promosi','MediaPromosiController');
-		Route::resource('pasar-aman','PasarAmanController');
-		Route::resource('pedoman','PedomanController');
-		Route::resource('sipaman','SipamanController');
-		Route::resource('peta-situs','PetaSitusController');
-		Route::resource('program','ProgramController');
-	});
+/* FRONT */
+Route::group(['namespace' => 'Front'], function(){
+	Route::resource('beranda','BerandaController');
+	Route::resource('faq','FAQController');
+	Route::resource('forum-fasilitator','ForumFasilitatorController');
+	Route::resource('galeri','GaleriController');
+	Route::resource('hubungi-kami','HubungiKamiController');
+	Route::resource('media-promosi','MediaPromosiController');
+	Route::resource('pasar-aman','PasarAmanController');
+	Route::resource('pedoman','PedomanController');
+	Route::resource('sipaman','SipamanController');
+	Route::resource('peta-situs','PetaSitusController');
+	Route::resource('program','ProgramController');
+});
 
+/* BACK */
+Route::group(['namespace' => 'Back'], function(){
+	Route::resource('admin', 'BackController');
 });
