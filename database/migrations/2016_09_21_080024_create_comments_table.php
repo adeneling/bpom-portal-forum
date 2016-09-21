af4c2b1ebd67ea38ee3b47e87c423d6b1227ff55
Rifky Ekayama <rifqykiw@gmail.com>
2016-09-21 15:01:57 +0700

    change table comment

---
+++ database/migrations/2016_09_21_080024_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('forum_user_id');
+            $table->unsignedInteger('thread_id');
+            $table->text('comment');
+            $table->timestamps();
+
+            $table->foreign('forum_user_id')->references('id')->on('forum_users')->onDelete('cascade');
+            $table->foreign('thread_id')->references('id')->on('threads')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
