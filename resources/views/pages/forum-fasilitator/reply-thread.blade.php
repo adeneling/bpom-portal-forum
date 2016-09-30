5ead8536303a9ac8f126529d1a711e4df567cd74
Rifky Ekayama <rifqykiw@gmail.com>
2016-09-30 10:23:40 +0700

    change style thread post

--- resources/views/pages/forum-fasilitator/reply-thread.blade.php
+++ resources/views/pages/forum-fasilitator/reply-thread.blade.php
@@ -74,44 +74,56 @@
 			</div>
 		</div>
 		<hr>
-		<ul class="media-list forum">
-
+		@if($comments != null)
 			@foreach($comments as $comment)
-				<li class="media well">
-					<div class="pull-left user-info" href="#">
-						<img class="avatar img-circle img-thumbnail" src="http://snipplicious.com/images/guest.png"
-							width="64" alt="Generic placeholder image">
-						<strong><a href="user.html">{{ $comment->forumUsers->name }}</a></strong>
-						<small>Member</small>
-						<br>
-						<small class="btn-group btn-group-xs">
-						<a class="btn btn-default"><i class="fa fa-thumbs-o-up"></i></a>
-						<a class="btn btn-default"><i class="fa fa-thumbs-o-down"></i></a>
-						<strong class="btn btn-success">+451</strong>
-						</small>
-					</div>
-					<div class="media-body">
-						<!-- Post Info Buttons -->
-						<div class="forum-post-panel btn-group btn-group-xs">
-							<a href="#" class="btn btn-default"><i class="fa fa-clock-o"></i> Posted 3 weeks ago</a>
-							<a href="#" class="btn btn-danger"><i class="fa fa-warning"></i> Report post</a>
+
+				<div class="row">
+					<div class="col-md-12">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								12 Agustus 2016 &nbsp;&nbsp;&nbsp;&nbsp; 18:30:19
+								<div class="pull-right">
+									#{{ $comment->counter+1 }}
+								</div>
+							</div>
+							<div class="panel-content">
+								<div class="row">
+									<div class="col-md-2" style="padding-top: 2em; padding-bottom: 2em;">
+										<center>
+											<div class="user-info" href="#">
+												<img class="avatar img-circle img-thumbnail" src="http://snipplicious.com/images/guest.png"
+													width="64" alt="Generic placeholder image"><br>
+												<strong><a href="user.html">{{ $comment->forumUsers->name }}</a></strong>
+												<small>Member</small>
+												<br>
+												<small class="btn-group btn-group-xs">
+												<a class="btn btn-default"><i class="fa fa-thumbs-o-up"></i></a>
+												<a class="btn btn-default"><i class="fa fa-thumbs-o-down"></i></a>
+												<strong class="btn btn-success">+451</strong>
+												</small>
+											</div>
+										</center>
+									</div>
+									<div class="col-md-10" style="padding: 2em 2em 2em 2em;">
+										{!! $comment->comment !!}
+									</div>
+								</div>
+							</div>
+							<div class="panel-footer">
+								&nbsp;
+								<div class="pull-right">
+									<a href="">Edit</a>
+									&nbsp;&nbsp;&nbsp;
+									<a href="">Reply</a>
+								</div>
+							</div>
 						</div>
-						<!-- Post Info Buttons END -->
-						<!-- Post Text -->
-						{!! $comment->comment !!}
-						<!-- Post Text EMD -->
 					</div>
-					<div class="media footer">
-						<div class="pull-right">
-							sdad
-						</div>
-					</div>
-				</li>
+				</div>
+
 			@endforeach
-
 			{{ $comments->links() }}
-
-		</ul>
+		@endif
 	</div>
 </div>
 @stop
--- resources/views/pages/forum-fasilitator/show.blade.php
+++ resources/views/pages/forum-fasilitator/show.blade.php
@@ -57,74 +57,103 @@
 		<div class="row">
 			<div class="col-md-12">
 
-				<ul class="media-list forum">
-					<li class="media well">
-						<div class="pull-left user-info" href="#">
-							<img class="avatar img-circle img-thumbnail" src="http://snipplicious.com/images/guest.png"
-								width="64" alt="Generic placeholder image">
-							<strong><a href="user.html">{{ $thread->forumUsers->name }}</a></strong>
-							<small>Member</small>
-							<br>
-							<small class="btn-group btn-group-xs">
-							<a class="btn btn-default"><i class="fa fa-thumbs-o-up"></i></a>
-							<a class="btn btn-default"><i class="fa fa-thumbs-o-down"></i></a>
-							<strong class="btn btn-success">+451</strong>
-							</small>
-							<hr>
-						</div>
-						<div class="media-body">
-							<!-- Post Info Buttons -->
-							<div class="forum-post-panel btn-group btn-group-xs">
-								<a href="#" class="btn btn-default"><i class="fa fa-clock-o"></i> Posted 3 weeks ago</a>
-								<a href="#" class="btn btn-danger"><i class="fa fa-warning"></i> Report post</a>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								12 Agustus 2016 &nbsp;&nbsp;&nbsp;&nbsp; 18:30:19
+								<div class="pull-right">
+									#1
+								</div>
 							</div>
-							<!-- Post Info Buttons END -->
-							<!-- Post Text -->
-							{!! $thread->konten !!}
-							<!-- Post Text EMD -->
+							<div class="panel-content">
+								<div class="row">
+									<div class="col-md-2" style="padding-top: 2em; padding-bottom: 2em;">
+										<center>
+											<div class="user-info" href="#">
+												<img class="avatar img-circle img-thumbnail" src="http://snipplicious.com/images/guest.png"
+													width="64" alt="Generic placeholder image"><br>
+												<strong><a href="user.html">{{ $thread->forumUsers->name }}</a></strong>
+												<small>Member</small>
+												<br>
+												<small class="btn-group btn-group-xs">
+												<a class="btn btn-default"><i class="fa fa-thumbs-o-up"></i></a>
+												<a class="btn btn-default"><i class="fa fa-thumbs-o-down"></i></a>
+												<strong class="btn btn-success">+451</strong>
+												</small>
+											</div>
+										</center>
+									</div>
+									{{-- <div class="col-md-10" style="padding-top: 2em; padding-bottom: 2em; padding-left: 2em; padding-right: 2em;"> --}}
+									<div class="col-md-10" style="padding: 2em 2em 2em 2em;">
+										{!! $thread->konten !!}
+									</div>
+								</div>
+							</div>
+							<div class="panel-footer">
+								&nbsp;
+								<div class="pull-right">
+									<a href="">Edit</a>
+									&nbsp;&nbsp;&nbsp;
+									<a href="">Reply</a>
+								</div>
+							</div>
 						</div>
-					</li>
+					</div>
+				</div>
 
-					<hr>
+				<hr>
 
-					@if($comments != null)
-						@foreach($comments as $comment)
+				@if($comments != null)
+					@foreach($comments as $comment)
 
-							<li class="media well">
-								<div class="pull-left user-info" href="#">
-									<img class="avatar img-circle img-thumbnail" src="http://snipplicious.com/images/guest.png"
-										width="64" alt="Generic placeholder image">
-									<strong><a href="user.html">{{ $comment->forumUsers->name }}</a></strong>
-									<small>Member</small>
-									<br>
-									<small class="btn-group btn-group-xs">
-									<a class="btn btn-default"><i class="fa fa-thumbs-o-up"></i></a>
-									<a class="btn btn-default"><i class="fa fa-thumbs-o-down"></i></a>
-									<strong class="btn btn-success">+451</strong>
-									</small>
-								</div>
-								<div class="media-body">
-									<!-- Post Info Buttons -->
-									<div class="forum-post-panel btn-group btn-group-xs">
-										<a href="#" class="btn btn-default"><i class="fa fa-clock-o"></i> Posted 3 weeks ago</a>
-										<a href="#" class="btn btn-danger"><i class="fa fa-warning"></i> Report post</a>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="panel panel-primary">
+									<div class="panel-heading">
+										12 Agustus 2016 &nbsp;&nbsp;&nbsp;&nbsp; 18:30:19
+										<div class="pull-right">
+											#{{ $comment->counter+1 }}
+										</div>
 									</div>
-									<!-- Post Info Buttons END -->
-									<!-- Post Text -->
-									{!! $comment->comment !!}
-									<!-- Post Text EMD -->
-								</div>
-								<div class="media footer">
-									<div class="pull-right">
-										sdad
+									<div class="panel-content">
+										<div class="row">
+											<div class="col-md-2" style="padding-top: 2em; padding-bottom: 2em;">
+												<center>
+													<div class="user-info" href="#">
+														<img class="avatar img-circle img-thumbnail" src="http://snipplicious.com/images/guest.png"
+															width="64" alt="Generic placeholder image"><br>
+														<strong><a href="user.html">{{ $comment->forumUsers->name }}</a></strong>
+														<small>Member</small>
+														<br>
+														<small class="btn-group btn-group-xs">
+														<a class="btn btn-default"><i class="fa fa-thumbs-o-up"></i></a>
+														<a class="btn btn-default"><i class="fa fa-thumbs-o-down"></i></a>
+														<strong class="btn btn-success">+451</strong>
+														</small>
+													</div>
+												</center>
+											</div>
+											<div class="col-md-10" style="padding: 2em 2em 2em 2em;">
+												{!! $comment->comment !!}
+											</div>
+										</div>
+									</div>
+									<div class="panel-footer">
+										&nbsp;
+										<div class="pull-right">
+											<a href="">Edit</a>
+											&nbsp;&nbsp;&nbsp;
+											<a href="">Reply</a>
+										</div>
 									</div>
 								</div>
-							</li>
-						@endforeach
-						{{ $comments->links() }}
-					@endif
+							</div>
+						</div>
 
-				</ul>
+					@endforeach
+					{{ $comments->links() }}
+				@endif
 
 			</div>
 		</div>
