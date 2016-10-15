507cc301b418b2a4aa57e80203e02342a7d0b11d
Rifky Ekayama <rifqykiw@gmail.com>
2016-10-15 08:16:04 +0700

    update

--- resources/views/pages/backend/pasar-aman/create.blade.php
+++ resources/views/pages/backend/pasar-aman/create.blade.php
@@ -23,7 +23,44 @@
 <!--start container-->
 	<div class="container">
 		<div class="section">
-		
+		<!--Input fields-->
+			<div id="input-fields">
+				<div class="row">
+					<form class="col s12">
+					{!! Form::open(['route' => ['admin.pasar-aman.store'], 'class' => 'col s12', 'files' => true]) !!}
+						<div class="row">
+							<div class="input-field col s12">
+								{!! Form::text('nama_pasar', old('nama_pasar'), ['id' => 'nama_pasar', 'class' => 'validate', 'placeholder' => 'Nama Pasar']) !!}
+								<label for="nama_pasar">Nama Pasar</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+								{!! Form::text('kepala_pasar', old('kepala_pasar'), ['id' => 'kepala_pasar', 'class' => 'validate', 'placeholder' => 'Kepala Pasar']) !!}
+								<label for="kepala_pasar">Kepala Pasar</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+								{!! Form::text('petugas_pasar', old('petugas_pasar'), ['id' => 'petugas_pasar', 'class' => 'validate', 'placeholder' => 'Petugas Pasar']) !!}
+								<label for="petugas_pasar">Petugas Pasar</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+								{!! Form::text('petugas_pasar', old('petugas_pasar'), ['id' => 'petugas_pasar', 'class' => 'validate', 'placeholder' => 'Petugas Pasar']) !!}
+								<label for="petugas_pasar">Petugas Pasar</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+								<input id="email" type="email" class="validate">
+								<label for="email">Email</label>
+							</div>
+						</div>
+					{!! Form::close() !!}
+				</div>
+			</div>
 		</div>
 	</div>
 <!--end container-->
