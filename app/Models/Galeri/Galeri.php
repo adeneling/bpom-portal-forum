c71c9b5ac1c67e3268f5987f65b26785d39ba65b
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-10-16 08:09:07 +0700

    migration galeri

--- app/Models/Galeri/Galeri.php
+++ app/Models/Galeri/Galeri.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Galeri extends Model
 {
-    //
+    protected $table = 'beritas';
+
+    protected $fillable = [
+        'judul', 
+        'konten',
+        'keterangan',
+        'media_foto', 
+    ];
 }
