bccb388e243f46ee16381e9d91273ce2bb79ae74
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-10-24 21:02:44 +0700

    update dashboard admin

--- resources/views/pages/backend/dashboard.blade.php
+++ resources/views/pages/backend/dashboard.blade.php
@@ -5,30 +5,25 @@
 @endsection
 
 @section('content')
-	<!--breadcrumbs start-->
-	<div id="breadcrumbs-wrapper">
-		<!-- Search for small screen -->
-		<div class="header-search-wrapper grey hide-on-large-only">
-			<i class="mdi-action-search active"></i>
-			<input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Explore Materialize">
-		</div>
-		<div class="container">
-			<div class="row">
-				<div class="col s12 m12 l12">
-					<h5 class="breadcrumbs-title">{{ $title }}</h5>
-					{!! Breadcrumbs::render('admin.dashboard') !!}
-				</div>
+<!--breadcrumbs start-->
+<div id="breadcrumbs-wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m12 l12">
+				<h5 class="breadcrumbs-title">{!! Breadcrumbs::render('admin.dashboard') !!}</h5>
+				
 			</div>
 		</div>
 	</div>
-	<!--breadcrumbs end-->
-	<!--start container-->
-	<div class="container">
-		<div class="section">
-			<center>
-				<img src="{{ asset('assets/frontend/img/logo-bpom.png') }}" class="img-responsive" alt="Image Intro">
-			</center>			
-		</div>
+</div>
+<!--breadcrumbs end-->
+<!--start container-->
+<div class="container">
+	<div class="section">
+		<center>
+			<img src="{{ asset('assets/frontend/img/logo-bpom.png') }}" class="img-responsive" alt="Image Intro">
+		</center>			
 	</div>
-	<!--end container-->
+</div>
+<!--end container-->
 @endsection
