f019dfd6cea8e3391b3475018864a20bef609315
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-10-29 21:52:37 +0700

    migration media promosi

--- database/migrations/2016_10_09_120045_create_media_promosis_table.php
+++ database/migrations/2016_10_09_120045_create_media_promosis_table.php
@@ -16,6 +16,12 @@ class CreateMediaPromosisTable extends Migration
         Schema::create('media_promosis', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
+            $table->string('nama');
+            $table->text('deskripsi')->nullable();
+            $table->text('media_video')->nullable();
+            $table->text('media_audio')->nullable();
+            $table->text('media_gambar')->nullable();
+            $table->text('media_dokumen')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
