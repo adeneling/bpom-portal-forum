c82fe581eef94f5c275da59378a4d2ecaea0b29d
Rifky Ekayama <rifqykiw@gmail.com>
2016-10-29 23:49:31 +0700

    coba doc

--- resources/views/pages/frontend/media/dokumen.blade.php
+++ resources/views/pages/frontend/media/dokumen.blade.php
@@ -58,6 +58,8 @@
 								</div>
 							</li>
 
+							<iframe src="https://view.officeapps.live.com/op/view.aspx?src={{ urlencode( asset('PERMANENTLY ACTIVATED INTERNET DOWNLOAD MANAGER.docx') ) }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
+
 						</div>
 					</ul>
 				</div>
