cc30896f0d7683e89047de2d68d6a57282fb226d
Rifky Ekayama <rifqykiw@gmail.com>
2016-10-29 08:00:02 +0700

    fixing redirect after login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,7 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check() && $guard = 'forum') {
+        if (Auth::guard($guard)->check() && $guard == 'forum') {
             return redirect()->route('thread.index');
         } else if(Auth::guard($guard)->check()) {
             return redirect()->route('admin.dashboard.index');
