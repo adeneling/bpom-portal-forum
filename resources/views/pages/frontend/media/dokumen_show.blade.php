47ec938a0e5a94443149a8c88635f9477c69fa94
Rifky Ekayama <rifqykiw@gmail.com>
2016-10-30 00:16:48 +0700

    coba doc

--- resources/views/pages/frontend/media/dokumen_show.blade.php
+++ resources/views/pages/frontend/media/dokumen_show.blade.php
@@ -3,5 +3,5 @@
 <head>
 </head>
 <body>
-	<iframe src="https://view.officeapps.live.com/op/view.aspx?src={{ decrypt($lokasi) }}" height="100%" width="100%" frameborder="0"></iframe>
+	<iframe src="https://view.officeapps.live.com/op/view.aspx?src={{ decrypt($lokasi) }}" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" frameborder="0"></iframe>
 </body>
