3957e8240a08e6d493f1457e0e252a371b0857a2
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-11-01 12:35:16 +0700

    pedoman controller

--- app/Http/Controllers/Back/Pedoman/PedomanController.php
+++ app/Http/Controllers/Back/Pedoman/PedomanController.php
@@ -8,7 +8,7 @@ use Storage;
 use App\Http\Requests;
 use App\Http\Requests\Pedoman\PedomanRequest;
 use App\Http\Controllers\Controller;
-use App\Models\Requests\Pedoman\Pedoman;
+use App\Models\Pedoman\Pedoman;
 
 class PedomanController extends Controller
 {
