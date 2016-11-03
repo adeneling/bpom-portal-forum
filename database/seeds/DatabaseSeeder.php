86433152aa456c759c4c5468e47773b1dcced1d3
Rifky Ekayama <rifqykiw@gmail.com>
2016-11-03 11:16:52 +0700

    nyalain seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,15 +12,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
     	$this->call(FirstRunSeeder::class);
-    	// $this->call(ProvinceSeeder::class);
-    	// $this->call(CitySeeder::class);
-    	// $this->call(SubdistrictSeeder::class);
-     //    $this->call(BannerSeeder::class);
-     //    $this->call(PasarAmanSeeder::class);
-     //    $this->call(PasarAmanBaruSeeder::class);
-     //    $this->call(BeritaSeeder::class);
-     //    $this->call(MediaPromosiSeeder::class);
-     //    $this->call(BeritaBPOMSeeder::class);
+    	$this->call(ProvinceSeeder::class);
+    	$this->call(CitySeeder::class);
+    	$this->call(SubdistrictSeeder::class);
+        $this->call(BannerSeeder::class);
+        $this->call(PasarAmanSeeder::class);
+        $this->call(PasarAmanBaruSeeder::class);
+        $this->call(BeritaSeeder::class);
+        $this->call(MediaPromosiSeeder::class);
+        $this->call(BeritaBPOMSeeder::class);
         $this->call(PedomanSeeder::class);
         $this->call(GaleriSeeder::class);
     }
