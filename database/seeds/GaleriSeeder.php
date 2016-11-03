4862ee0581bb0842c3402b55ca05c108e480813c
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-11-03 10:10:38 +0700

    fix galeri seeder

--- database/seeds/GaleriSeeder.php
+++ database/seeds/GaleriSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Models\Galeri\Galeri;
+use Illuminate\Support\Facades\Storage;
 class GaleriSeeder extends Seeder
 {
     /**
@@ -11,6 +12,132 @@ class GaleriSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Storage::makeDirectory('galeri/foto');
+
+		$path = public_path('seeder/galeri/kampanye pasar aman di pasar kutowinangun3.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Kampanye pasar aman di pasar Kutowinangun';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar agus salim pekan baru.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Agus Salim Pekan Baru';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar alai padang.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Alai Padang';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/Pasar bawah Bukittinggi.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Bawah Bukit Tinggi';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/Pasar bawah Bukittinggi2.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Bawah Bukit Tinggi';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar keutapang.png');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Keutapang';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar lambaro.JPG');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Lambaro';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar pangururan.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Pangururan';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar peunayong.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Peunayong';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar sei sikambing medan.gif');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Sei Sikambing Medan';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar tanah kongsi.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Tanah Kongsi';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar tanah kongsi2 putri indonesia.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Tanah Kongsi Putri Indonesia';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar tanah kongsi2.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Tanah Kongsi';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
+
+		$path = public_path('seeder/galeri/pasar ulak karang padang.jpg');
+		$filename = 'galeri/foto/'.str_random(10).'.'.explode('.', $path)[1];
+		Storage::put($filename, file_get_contents($path));
+		$galeri = new Galeri;
+		$galeri->user_id = 1;
+		$galeri->judul = 'Pasar Ulak Karang Padang';
+		$galeri->foto = !is_null($filename) ? Storage::url($filename) : '';
+		$galeri->save();
     }
 }
