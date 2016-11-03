9bf0df4120617fc373f3e45752c050a741c9f722
Maulana Yusup Abdullah <adeneling@gmail.com>
2016-11-03 14:09:57 +0700

    update bahan berbahaya

--- resources/views/pages/frontend/bahan-berbahaya/index.blade.php
+++ resources/views/pages/frontend/bahan-berbahaya/index.blade.php
@@ -14,16 +14,14 @@
 <!-- PAGE CONTENT -->
 <div class="page-content">
 	<div class="container">
-		<!-- TOP FAQs -->
-		<h2>PROGRAM PASAR AMAN DARI BAHAN BERBAHAYA</h2>
-		<div class="panel-group panel-group-faq top-faq">
-			<div class="panel panel-minimal">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion" href="#top-faq1" class="collapsed"><span class="number pull-left">1</span> Apa yang dimaksud dengan bahan berbahaya?</a>
+						<a data-toggle="collapse" data-parent="#accordion" href="#1" class="collapsed">Apa yang dimaksud dengan bahan berbahaya?<i class="fa fa-angle-down pull-right icon-collapsed"></i><i class="fa fa-angle-up pull-right icon-expanded"></i></a>
 					</h4>
 				</div>
-				<div id="top-faq1" class="panel-collapse collapse" style="height: 0px;">
+				<div id="1" class="panel">
 					<div class="panel-body">
 						<p align="justify">Bahan berbahaya adalah bahan kimia baik dalam bentuk tunggal maupun campuran yang dapat membahayakan kesehatan dan lingkungan hidup secara langsung atau tidak langsung yang mempunyai sifat racun, karsinogenik, teratogenik, mutagenik, korosif dan iritasi <strong>(Peraturan Menteri Kesehatan Nomor 472/Menkes/Per/V/1996 tentang Pengamanan Bahan Berbahaya Bagi Kesehatan).</strong><br>
 						Sesungguhnya bahan kimia bersifat esensial dalam peningkatan kesejahteraan manusia, dan penggunaannya sedemikian luas di berbagai sektor antara lain industri, pertanian, pertambangan dan lain sebagainya. Namun hal yang perlu kita waspadai adalah adanya kecenderungan penggunaan yang salah sejumlah bahan berbahaya pada pangan.
@@ -31,14 +29,16 @@
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<div class="panel panel-minimal">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion" href="#top-faq2" class="collapsed"><span class="number pull-left">2</span> Apa saja yang termasuk bahan berbahaya yang ditemukan dalam pangan?</a>
+						<a data-toggle="collapse" data-parent="#accordion" href="#2" class="collapsed">Apa saja yang termasuk bahan berbahaya yang ditemukan dalam pangan?<i class="fa fa-angle-down pull-right icon-collapsed"></i><i class="fa fa-angle-up pull-right icon-expanded"></i></a>
 					</h4>
 				</div>
-				<div id="top-faq2" class="panel-collapse collapse" style="height: 0px;">
+				<div id="2" class="panel">
 					<div class="panel-body">
 						<p align="justify">
 							Pada dasarnya ada banyak bahan berbahaya yang tidak boleh ditambahkan kedalam makanan, namun dari hasil pengawasan, ada 4 bahan berbahaya yang sering ditemukan disalahgunakan dalam makanan yaitu Boraks, Formalin, Rhodamin B, dan Kuning metanil.
@@ -50,14 +50,16 @@
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<div class="panel panel-minimal">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion" href="#top-faq3" class="collapsed"><span class="number pull-left">3</span> Apa fungsi sebenarnya dari keempat bahan tersebut?</a>
+						<a data-toggle="collapse" data-parent="#accordion" href="#3" class="collapsed">Apa fungsi sebenarnya dari keempat bahan tersebut?<i class="fa fa-angle-down pull-right icon-collapsed"></i><i class="fa fa-angle-up pull-right icon-expanded"></i></a>
 					</h4>
 				</div>
-				<div id="top-faq3" class="panel-collapse collapse" style="height: 0px;">
+				<div id="3" class="panel">
 					<div class="panel-body">
 						<p align="justify">
 							Boraks merupakan senyawa dengan nama kimia natrium tetraborat yang berbentuk kristal lunak. Boraks digunakan untuk campuran pembuatan gelas, pengawet kayu, salep kulit, campuran pupuk tanaman, dll.<br>
@@ -67,14 +69,16 @@
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<div class="panel panel-minimal">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion" href="#top-faq4" class="collapsed"><span class="number pull-left">4</span>Apa saja contoh penyalahgunaan bahan berbahaya dalam pangan dan apa ciri-cirinya?</a>
+						<a data-toggle="collapse" data-parent="#accordion" href="#4" class="collapsed">Apa saja contoh penyalahgunaan bahan berbahaya dalam pangan dan apa ciri-cirinya?<i class="fa fa-angle-down pull-right icon-collapsed"></i><i class="fa fa-angle-up pull-right icon-expanded"></i></a>
 					</h4>
 				</div>
-				<div id="top-faq4" class="panel-collapse collapse" style="height: 0px;">
+				<div id="4" class="panel">
 					<div class="panel-body">
 						<p align="justify">
 							<ol type="a">
@@ -86,20 +90,21 @@
 									<strong>Ciri – cirinya:</strong> Warna merah mencolok, rasa pahit, jika dipegang warna merah akan menempel di kulit.</li>
 								<li>Contoh pangan yang banyak ditemukan mengandung kuning metanil : kerupuk berwarna kuning, mie basah, tahu kuning. <br>
 									<strong>Ciri – cirinya:</strong> warna kuning mencolok, rasa pahit, jika dipegang warna kuning akan menempel di kulit.</li>
-
 							</ol>
 						</p>
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<div class="panel panel-minimal">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion" href="#top-faq5" class="collapsed"><span class="number pull-left">5</span> Apa akibatnya jika kita mengkonsumsi bahan berbahaya?</a>
+						<a data-toggle="collapse" data-parent="#accordion" href="#5" class="collapsed">Apa akibatnya jika kita mengkonsumsi bahan berbahaya?<i class="fa fa-angle-down pull-right icon-collapsed"></i><i class="fa fa-angle-up pull-right icon-expanded"></i></a>
 					</h4>
 				</div>
-				<div id="top-faq5" class="panel-collapse collapse" style="height: 0px;">
+				<div id="5" class="panel">
 					<div class="panel-body">
 						<p align="justify">
 							<ol type="a">
@@ -112,14 +117,16 @@
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<div class="panel panel-minimal">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion" href="#top-faq6" class="collapsed"><span class="number pull-left">6</span> Kenapa bahan berbahaya tersebut disalahgunakan dalam pangan?</a>
+						<a data-toggle="collapse" data-parent="#accordion" href="#5" class="collapsed">Kenapa bahan berbahaya tersebut disalahgunakan dalam pangan?<i class="fa fa-angle-down pull-right icon-collapsed"></i><i class="fa fa-angle-up pull-right icon-expanded"></i></a>
 					</h4>
 				</div>
-				<div id="top-faq6" class="panel-collapse collapse" style="height: 0px;">
+				<div id="5" class="panel">
 					<div class="panel-body">
 						<p align="justify">
 							Terdapat berbagai faktor yang mendorong banyak pihak untuk melakukan praktek penggunaan yang salah bahan kimia terlarang untuk pangan. Pertama, bahan kimia tersebut mudah diperoleh di pasaran. Kedua, harganya relatif murah. Ketiga, pangan yang mengandung bahan tersebut menampakkan tampilan fisik yang memikat. Keempat, tidak menimbulkan efek negatif seketika. Kelima, informasi bahan berbahaya tersebut relatif terbatas, dan pola penggunaannya telah dipraktekkan secara turun-temurun.
@@ -127,10 +134,8 @@
 					</div>
 				</div>
 			</div>
-
-
-
 		</div>
+
 	</div>
 </div>
 <!-- END PAGE CONTENT -->
