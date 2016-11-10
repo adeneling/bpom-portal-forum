3a5080064beb2a7607dd0442020833d18bb3b5f1
Rifky Ekayama <rifqykiw@gmail.com>
2016-11-10 08:27:26 +0700

    tutup menu pasar aman

--- resources/views/layouts/backend/includes/left-sidebar.blade.php
+++ resources/views/layouts/backend/includes/left-sidebar.blade.php
@@ -52,9 +52,9 @@
 		<li class="bold {{ Request::segment(2) == 'media-promosi' ? 'active' : '' }}">
 			<a href="{{ route('admin.media-promosi.index') }}" class="waves-effect waves-cyan"><i class="mdi-action-perm-media"></i> Kelola Media Promosi</a>
 		</li>
-		<li class="bold {{ Request::segment(2) == 'pasar-aman' ? 'active' : '' }}">
+		{{-- <li class="bold {{ Request::segment(2) == 'pasar-aman' ? 'active' : '' }}">
 			<a href="{{ route('admin.pasar-aman.index') }}" class="waves-effect waves-cyan"><i class="mdi-action-wallet-travel"></i> Kelola Pasar Aman</a>
-		</li>
+		</li> --}}
 
 		<li class="bold {{ Request::segment(2) == 'pengawasan-pasar' ? 'active' : '' }}">
 			<a href="{{ route('admin.pengawasan-pasar.index') }}" class="waves-effect waves-cyan"><i class="mdi-action-assessment"></i> Pengawasan Pasar</a>
