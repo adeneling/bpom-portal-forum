9f52a419ef66a2b70dc5cdabf25176966fe8e5a0
Rifky Ekayama <rifqykiw@gmail.com>
2016-11-16 09:07:48 +0700

    pasar remote seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,21 +11,19 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$this->call(FirstRunSeeder::class);
-		$this->call(ProvinceSeeder::class);
-		$this->call(CitySeeder::class);
-		// $this->call(SubdistrictSeeder::class);
-		$this->call(BannerSeeder::class);
-		// $this->call(PasarAmanSeeder::class);
-		// $this->call(PasarAmanBaruSeeder::class);
+		// $this->call(FirstRunSeeder::class);
+		// $this->call(ProvinceSeeder::class);
+		// $this->call(CitySeeder::class);
+		// // $this->call(SubdistrictSeeder::class);
+		// $this->call(BannerSeeder::class);
 		$this->call(PasarAmanRemoteSeeder::class);
-		$this->call(BeritaSeeder::class);
-		$this->call(MediaPromosiSeeder::class);
-		$this->call(BeritaBPOMSeeder::class);
-		$this->call(PedomanSeeder::class);
-		$this->call(GaleriSeeder::class);
-		$this->call(ProgramSeeder::class);
-		$this->call(ProgramDokumenSeeder::class);
+		// $this->call(BeritaSeeder::class);
+		// $this->call(MediaPromosiSeeder::class);
+		// $this->call(BeritaBPOMSeeder::class);
+		// $this->call(PedomanSeeder::class);
+		// $this->call(GaleriSeeder::class);
+		// $this->call(ProgramSeeder::class);
+		// $this->call(ProgramDokumenSeeder::class);
 		// $this->call(ForumDummySeeder::class);
 	}
 }
